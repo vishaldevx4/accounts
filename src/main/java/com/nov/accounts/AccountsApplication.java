4d168ae138b7d8f0b71631c91acda98695dfc744
PulsatingStar <vishal.kalsi22@gmail.com>
2025-11-28 04:03:17 -0500

    added env configurations

--- src/main/java/com/nov/accounts/AccountsApplication.java
+++ src/main/java/com/nov/accounts/AccountsApplication.java
@@ -1,9 +1,12 @@
 package com.nov.accounts;
 
+import com.nov.accounts.dto.AccountsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = AccountsContactInfoDto.class)
 public class AccountsApplication {
 
     public static void main(String[] args) {
